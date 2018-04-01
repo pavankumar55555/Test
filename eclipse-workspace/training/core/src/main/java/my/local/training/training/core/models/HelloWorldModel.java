5c2781a9ee8adc16e53060c02d7c342cc6f26e18
Pavan Kumar Ainampudi <pavankumar55555@apple.com>
2018-04-01 21:19:48 +0530

    not working

--- eclipse-workspace/training/core/src/main/java/my/local/training/training/core/models/HelloWorldModel.java
+++ eclipse-workspace/training/core/src/main/java/my/local/training/training/core/models/HelloWorldModel.java
@@ -37,7 +37,7 @@ public class HelloWorldModel {
 
     @PostConstruct
     protected void init() {
-        message = "\tHello World modified java file \n";
+        message = "\tHello World modified java fileeee \n";
         message += "\tThis is instance: " + settings.getSlingId() + "\n";
         message += "\tResource type is: " + resourceType + "\n";
     }
